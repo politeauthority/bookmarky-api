f6e728567a2f3242661df6b7d5984b577d925c40
Alix Fullerton <fullertonalix0@gmail.com>
2024-07-31 23:15:24 -0600

    Fixing issues with containers running:

--- src/bookmarky/migrations/data/sql/up/1.sql
+++ src/bookmarky/migrations/data/sql/up/1.sql
@@ -31,12 +31,9 @@ CREATE TABLE IF NOT EXISTS users (
     last_access TIMESTAMP
 );
 
-<<<<<<< HEAD
 ---
 --- Create roles
 ---
-=======
->>>>>>> ecf2a33 (Bootstrap (#1))
 CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -46,11 +43,7 @@ CREATE TABLE IF NOT EXISTS roles (
 );
 
 ---
-<<<<<<< HEAD
---- Create perms
-=======
---- Create role_perms
->>>>>>> ecf2a33 (Bootstrap (#1))
+
 ---
 CREATE TABLE IF NOT EXISTS perms (
     id SERIAL PRIMARY KEY,
@@ -84,8 +77,4 @@ CREATE TABLE IF NOT EXISTS bookmarks (
     url VARCHAR,
     deleted BOOLEAN DEFAULT False,
     UNIQUE (user_id, url)
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> ecf2a33 (Bootstrap (#1))
