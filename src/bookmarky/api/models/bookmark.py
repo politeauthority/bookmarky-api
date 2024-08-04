52d006328ef310d822d92db7acdab0d3a10acc81
Alix <fullertonalix0@gmail.com>
2024-08-03 21:16:17 -0600

    v0.0.1.1 (#13)
    Working proof of concept!

--- src/bookmarky/api/models/bookmark.py
+++ src/bookmarky/api/models/bookmark.py
@@ -3,7 +3,7 @@
     Model Bookmark
 
 """
-from bookmarky.shared.models.bookmark import FIELD_MAP
+from bookmarky.shared.models.bookmark import FIELD_MAP, FIELD_META
 from bookmarky.api.models.base_entity_meta import BaseEntityMeta
 
 
@@ -17,9 +17,21 @@ class Bookmark(BaseEntityMeta):
         """
         super(Bookmark, self).__init__(conn, cursor)
         self.field_map = FIELD_MAP
+        self.ux_key = FIELD_META["ux_key"]
         self.table_name = "bookmarks"
         self.immutable = False
         self.createable = True
         self.setup()
+
+    def __repr__(self):
+        """Bookmark model representation."""
+        if self.url and self.name:
+            return "<%s: %s - %s %s>" % (self.__class__.__name__, self.id, self.url, self.name)
+        elif self.url:
+            return "<%s: %s - %s>" % (self.__class__.__name__, self.id, self.url)
+        elif self.id:
+            return "<%s: %s>" % (self.__class__.__name__, self.id)
+        return "<%s>" % self.__class__.__name__
+
 
 # End File: politeauthority/bookmarky/src/bookmarky/api/models/bookmark.py
--- src/bookmarky/api/models/tag.py
+++ src/bookmarky/api/models/tag.py
@@ -3,7 +3,7 @@
     Model - Tag
 
 """
-from bookmarky.shared.models.tag import FIELD_MAP
+from bookmarky.shared.models.tag import FIELD_MAP, FIELD_META
 from bookmarky.api.models.base_entity_meta import BaseEntityMeta
 
 
@@ -17,9 +17,19 @@ class Tag(BaseEntityMeta):
         """
         super(Tag, self).__init__(conn, cursor)
         self.field_map = FIELD_MAP
+        self.ux_key = FIELD_META["ux_key"]
         self.table_name = "tags"
         self.immutable = True
         self.createable = True
         self.setup()
+
+    def __repr__(self):
+        """Tag model representation."""
+        if self.name:
+            return "<%s: %s - %s>" % (self.__class__.__name__, self.id, self.name)
+        elif self.id:
+            return "<%s: %s>" % (self.__class__.__name__, self.id)
+        return "<%s>" % self.__class__.__name__
+
 
 # End File: politeauthority/bookmarky/src/bookmarky/api/models/ctrl_tag.py
