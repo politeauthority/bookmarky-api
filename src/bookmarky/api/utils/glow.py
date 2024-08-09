072e049a2739270aa64b09f70a3054ec21f0d336
Alix Fullerton <fullertonalix0@gmail.com>
2024-08-09 15:47:47 -0600

    Working out issues with healthz

--- src/bookmarky/api/utils/glow.py
+++ src/bookmarky/api/utils/glow.py
@@ -28,10 +28,10 @@ options = {}
 # Collect General Details
 global general
 general = {
-    "LOG_LEVEL": os.environ.get("CVER_LOG_LEVEL", "INFO"),
+    "LOG_LEVEL": os.environ.get("LOG_LEVEL", "INFO"),
     "VERSION": version,
-    "CVER_BUILD": os.environ.get("CVER_BUILD"),
-    "CVER_BUILD_SHORT": "",
+    "BUILD": os.environ.get("BUILD"),
+    "BUILD_SHORT": "",
     "ENV": os.environ.get("BOOKMARKY_ENV"),
     "JWT_EXPIRE_MINUTES": os.environ.get("JWT_EXPIRE_MINUTES", 60),
     "SECRET_KEY": os.environ.get("SECRET_KEY", "hello-world123"),
@@ -39,12 +39,12 @@ general = {
     "LOG_HEALTH_CHECKS": os.environ.get("LOG_HEALTH_CHECKS", True),
     "CVER_DEPLOYED_AT": os.environ.get("CVER_DEPLOYED_AT", None)
 }
-if general["CVER_BUILD"]:
-    general["CVER_BUILD_SHORT"] = general["CVER_BUILD"][:12]
-if general["CVER_TEST"] == "true":
-    general["CVER_TEST"] = True
+if general["BUILD"]:
+    general["BUILD_SHORT"] = general["CVER_BUILD"][:12]
+if general["TEST"] == "true":
+    general["TEST"] = True
 else:
-    general["CVER_TEST"] = False
+    general["TEST"] = False
 if general["LOG_HEALTH_CHECKS"] == "true":
     general["LOG_HEALTH_CHECKS"] = True
 else:
