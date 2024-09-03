5dda5a8c3cf1997b2d41c44146f7409ded4f1d96
Alix Fullerton <fullertonalix0@gmail.com>
2024-09-02 19:20:10 -0600

    Adding POST for /bookmarks/meta

--- src/bookmarky/api/controllers/models/ctrl_bookmark.py
+++ src/bookmarky/api/controllers/models/ctrl_bookmark.py
@@ -36,10 +36,30 @@ def get_model(bookmark_id: int = None) -> Response:
 @ctrl_bookmark.route("", methods=["POST"])
 @ctrl_bookmark.route("/", methods=["POST"])
 @ctrl_bookmark.route("/<bookmark_id>", methods=["POST"])
+@ctrl_bookmark.route("/<bookmark_id>/", methods=["POST"])
 @auth.auth_request
 def post_model(bookmark_id: int = None):
     """POST operation for a Bookmark model.
     POST /bookmark
+    @todo: This needs to be locked down to only allow users to delete their own Tag relationships
+    """
+    data = {
+        "user_id": glow.user["user_id"]
+    }
+    logging.info("POST Bookmark")
+    r_args = api_util.get_post_data()
+    if r_args and "tag_id" in r_args:
+        BookmarkTag().create(bookmark_id, r_args["tag_id"])
+    if r_args and "tag_id_remove" in r_args:
+        BookmarkTag().remove(bookmark_id, r_args["tag_id_remove"])
+    return ctrl_base.post_model(Bookmark, bookmark_id, data)
+
+
+@ctrl_bookmark.route("/meta/<bookmark_id>", methods=["POST"])
+@auth.auth_request
+def post_model_meta(bookmark_id: int = None):
+    """POST operation for a Bookmark model metas.
+    POST /meta/bookmark
     @todo: This needs to be locked down to only allow users to delete their own Tag relationships
     """
     data = {
